5e356d2d5e9aaeb98eb8daf9d7d226c25a2a121d
Yurii <juretz32@yandex.ru>
2024-10-29 20:25:56 +0300

    Update description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject books-download "0.1.0-SNAPSHOT"
-  :description "Get books from website to epub"
+  :description "Books from website to epub"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.github.yurii-tov/selenium "0.1.0-SNAPSHOT"]]
