d5c4b3ea5a8d1af0b238fdcf5d112508582776fa
Yurii <juretz32@yandex.ru>
2025-04-29 15:09:50 +0300

    Remove unneeded dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject books-download "0.1.0-SNAPSHOT"
   :description "Books from website to epub"
   :repositories [["jitpack" "https://jitpack.io"]]
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.github.yurii-tov/selenium "0.1.0-SNAPSHOT"]]
+  :dependencies [[com.github.yurii-tov/selenium "0.1.0-SNAPSHOT"]]
   :repl-options {:init-ns books-download.core})
